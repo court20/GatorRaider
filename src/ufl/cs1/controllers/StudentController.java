aed345e763c70e814df47ca2bdd849c8d875f621
mohona17 <32102325+mohona17@users.noreply.github.com>
2017-12-04 16:50:20 -0500

    Mohona's code for normal mode

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -16,9 +16,50 @@ public final class StudentController implements DefenderController
 	{
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
+		Defender mohona = enemies.get(3);
 
-		//Insert unique ghost methods here
-		
+		actions[3] = mohonaAction(mohona, game, timeDue);
+
+
 		return actions;
+	}
+
+	public int mohonaAction(Defender mohona, Game game, long timeDue) {
+		int mohonasMove = -1;
+		int i = 0;
+		int min = 0;
+		int minNode = 0;
+		int distance = 0;
+		int j = 0;
+
+		if (game.getPowerPillList().size() != 0) {
+			//Get lists of pills
+			for (i = 1; i < game.getPowerPillList().size() && i > 0 ; i++) {
+				//ensure that there is a power pill at the node
+				if (game.checkPowerPill(game.getPowerPillList().get(i)) && game.checkPowerPill(game.getPowerPillList().get(j))) {
+					//Find the pill pacman is nearest to.
+					if (i == 1) {
+						min = game.getAttacker().getLocation().getPathDistance(game.getPowerPillList().get(j));
+						minNode = j;
+					}
+					distance = game.getAttacker().getLocation().getPathDistance(game.getPowerPillList().get(i));
+					if (distance < min) {
+						min = distance;
+						minNode = i;
+					}
+				}
+				else{
+					j++;
+				}
+			}
+			//Move towards that pill
+			mohonasMove = mohona.getNextDir(game.getPowerPillList().get(minNode), true);
+		}
+		//no pills, chase attacker.
+		else {
+			mohonasMove = mohona.getNextDir(game.getAttacker().getLocation(), true);
+		}
+
+		return mohonasMove;
 	}
 }
